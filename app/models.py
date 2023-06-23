077cd8e1fa985f02347362751b74322c7f60b98c
askutukova <137469147+askutukova@users.noreply.github.com>
2023-06-23 12:50:26 +0900

    Initial commit

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Category(db.Model):
+    pass
+
+
+class Article(db.Model):
+    pass
---
+++ app/views.py
@@ -0,0 +1,7 @@
+
+def index_page():
+    pass
+
+
+def article_page():
+    pass
